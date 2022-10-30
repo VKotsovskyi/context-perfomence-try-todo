81a5ce2e04cd9724555dcbf5bb449b4275dacbba
Vasyl Kotsovskyi <kotsovskyi@gmail.com>
2022-10-30 17:21:13 +0200

    Change color names

--- src/components/todo/styled.ts
+++ src/components/todo/styled.ts
@@ -6,12 +6,12 @@ export const TodoContainer = styled.div`
   h1 {
     font-size: 2em;
     margin-bottom: 0;
-    color: ${colors.darkBlue};
+    color: ${colors.primary};
   }
   p {
     width: 50%;
     margin: 5px auto;
-    color: ${colors.darkGrey};
+    color: ${colors.primaryFont};
     font-family: monospace;
   }
 `
@@ -19,20 +19,20 @@ export const TodoContainer = styled.div`
 export const ActionButton = styled.button`
   padding: 11px 21px;
   color: ${colors.white};
-  background: ${colors.green};
+  background: ${colors.action};
   border-radius: 5px;
   border: none;
   font-weight: bold;
   font-size: 1.1em;
   cursor: pointer;
-  box-shadow: -4px 6px 16px -7px ${colors.darkBlue};
+  box-shadow: -4px 6px 16px -7px ${colors.primary};
 `
 
 export const Input = styled.input`
   padding: 10px 20px; 
   border-radius: 5px;
   margin-right: 10px;
-  border: 2px solid ${colors.darkBlue};
+  border: 2px solid ${colors.primary};
   font-size: 1.1em;
 `
 
@@ -46,9 +46,9 @@ export const ListItem = styled.li`
   list-style-type: none;
   padding: 20px;
   font-size: 1.5em;
-  color: ${colors.darkBlue};
-  border: 2px solid ${colors.lightBlue};
+  color: ${colors.primary};
+  border: 2px solid ${colors.secondary};
   margin-bottom: 10px;
-  box-shadow: -4px 6px 16px -7px ${colors.darkBlue};
+  box-shadow: -4px 6px 16px -7px ${colors.primary};
   text-align: left;
 `
